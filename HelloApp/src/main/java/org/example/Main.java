567bec0423efc996b9394daa93cbed457252d4b5
immu27 <immusri27@gmail.com>
2026-03-26 13:51:13 +0530

    Coded UC2: Personalized greeting for Immanuel

--- HelloApp/src/main/java/org/example/Main.java
+++ HelloApp/src/main/java/org/example/Main.java
@@ -3,7 +3,13 @@ package org.example;
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
-    static void main() {
-        System.out.println("Hello World");
+    public static void main(String[] args) {
+        if (args.length > 0) {
+            // This line runs if you put "Immanuel" in the box
+            System.out.println("Hello, " + args[0] + "!");
+        } else {
+            // This line runs if the box is empty
+            System.out.println("Hello, World!");
+        }
     }
 }
