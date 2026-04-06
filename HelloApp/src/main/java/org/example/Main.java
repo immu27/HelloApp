6ab90463c7c608dca7357eb4655bc3e2303e15e5
immu27 <immusri27@gmail.com>
2026-04-06 13:08:20 +0530

    Coded UC5: Display Hello with multiple args using enhanced for loop

--- HelloApp/src/main/java/org/example/Main.java
+++ HelloApp/src/main/java/org/example/Main.java
@@ -1,22 +1,29 @@
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public static void main(String[] args) {
-    String name = "World";
+    String finalNames;
 
-    if (args.length > 0) {
+    // Check if any arguments were passed
+    if (args.length == 0) {
+        finalNames = "World";
+    } else {
         StringBuilder nameBuilder = new StringBuilder();
+        boolean first = true;
 
-        // Loop through all arguments provided
-        for (int i = 0; i < args.length; i++) {
-            nameBuilder.append(args[i]);
-
-            // Add a comma and space ONLY if it's not the last name
-            if (i < args.length - 1) {
+        // The "Enhanced For Loop" - it reads "for each String 'name' in 'args'"
+        for (String name : args) {
+            // Only add a comma if this is NOT the very first name we process
+            if (!first) {
                 nameBuilder.append(", ");
             }
+            nameBuilder.append(name);
+
+            // After the first name is added, set this to false so
+            // the NEXT names get a comma
+            first = false;
         }
-        name = nameBuilder.toString();
+        finalNames = nameBuilder.toString();
     }
 
-    System.out.println("Hello, " + name + "!");
+    System.out.println("Hello, " + finalNames + "!");
 }
