505d230cdedf3394531938e01269dc99d4346a9c
immu27 <immusri27@gmail.com>
2026-04-06 13:13:27 +0530

    Coded UC7: Display Hello with multiple args using String.join() method

--- HelloApp/src/main/java/org/example/Main.java
+++ HelloApp/src/main/java/org/example/Main.java
@@ -1,27 +1,16 @@
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public static void main(String[] args) {
-    String finalNames;
+    String names;
 
+    // If no names are given, default to "World"
     if (args.length == 0) {
-        finalNames = "World";
+        names = "World";
     } else {
-        StringBuilder nameBuilder = new StringBuilder();
-
-        // 1. Add EVERY name + a comma and space
-        for (String name : args) {
-            nameBuilder.append(name).append(", ");
-        }
-
-        // 2. Cleanup: If we added names, chop off the last ", "
-        if (nameBuilder.length() > 0) {
-            // substring(start, end) takes a slice of the text
-            // We go from 0 to (Total Length - 2)
-            finalNames = nameBuilder.substring(0, nameBuilder.length() - 2);
-        } else {
-            finalNames = "";
-        }
+        // String.join takes the "glue" first, then the array of names
+        // It automatically handles all commas perfectly!
+        names = String.join(", ", args);
     }
 
-    System.out.println("Hello, " + finalNames + "!");
+    System.out.println("Hello, " + names + "!");
 }
